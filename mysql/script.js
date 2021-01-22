566e2397c59c6640018b38b015137ebbae4f0ff2
mararbia <mararbiaoliveira@gmail.com>
2021-01-22 20:36:58 -0300

    Connection DB - NodeJS whit MySQL (Xampp)

---
+++ mysql/script.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'cardapiovirtual'
+});
+
+connection.connect(function(error){
+    if(!!error){
+        console.log('Error');
+    } else {
+        console.log("Connected");
+    }
+});
+
+app.get('/', function(req, resp){
+    connection.query("SELECT * FROM usuarios", function(error, rows, fields){
+        if(!!error){
+            console.log('Error in the query');
+        } else {
+            console.log('Successful query');
+            console.log(rows);
+        }
+    });
+})
+
+app.listen(1337);
